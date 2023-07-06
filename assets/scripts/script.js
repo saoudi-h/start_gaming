10bb8f02775eaba3563d2aa288cc7b3892f9348a
Hakim Saoudi <saoudihakim@gmai.com>
2023-07-06 17:12:10 +0200

    navbar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
 
-import $ from 'jquery';
-$('body').prepend('<h1>' + 'Titi' + '</h1>');
-import greet from './greet';
+// import $ from 'jquery';
+// $('body').prepend('<h1>' + 'Titi' + '</h1>');
+// import greet from './greet';
